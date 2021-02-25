7578a07349c8052e24341f264e50fb589c9a2294
Tural Kerimov <turaltrueandfalse@gmail.com>
2021-02-25 23:52:12 +0300

    form submit file

--- keys/index.js
+++ keys/index.js
@@ -3,8 +3,10 @@ module.exports = {
     // SESSION_SECRET: 'some secret value',
     SENDGRID_API_KEY: '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',
     EMAIL_FROM: 'turaltrueandfalse@gmail.com',
+
     // EMAIL_OFFICE: 'turaltrueandfalse@gmail.com',
     EMAIL_OFFICE: 'ecosystem.vent@gmail.com',
+
     PHONE_OFFICE: '+7(843)505-27-75, +7(939)505-27-75 с 8:00 до 20:00',
     ADDRESS_OFFICE: 'г. Казань, Оренбургский тракт, 10 км, стр. 1',
     BASE_URL: 'https://ecosystem-vent.ru'
