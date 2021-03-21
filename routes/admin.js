0e79528d7a5446f4dd02e1e047541cead56c68d2
Tural Kerimov <turaltrueandfalse@gmail.com>
2021-03-21 19:50:23 +0300

    database

--- routes/admin.js
+++ routes/admin.js
@@ -117,13 +117,13 @@ function getFilesHandler(files, keyBody) {
     return result;
 }
 
-async function fileUpload(file, direction) {
+function fileUpload(file, direction) {
     const extension = path.extname(file.name);
     const name = path.basename(file.name, extension).toLowerCase();
     const fullPath = direction + file.md5 + extension;
 
     const absolutePath = path.resolve('./public/assets/product/img-db');
-    await sharp(file.data)
+    sharp(file.data)
         .resize(null, 300)
         .toFile(absolutePath + '/' + file.md5 + extension, (err, info) => {
             if (err) console.log(err);
