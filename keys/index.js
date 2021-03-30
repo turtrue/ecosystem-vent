b6ea2378d3b3d7a1b9fce935a02a4b477e28bf76
Tural Kerimov <turaltrueandfalse@gmail.com>
2021-03-30 19:22:54 +0300

    edit keys.js

--- keys/index.js
+++ keys/index.js
@@ -1,7 +1,7 @@
 module.exports = {
     MONGODB_URI: 'mongodb+srv://turtrue:A7Vth89fJ0GSZGtn@cluster0.bq5d1.mongodb.net/ecosystem',
     SESSION_SECRET: 'some secret value',
-    SENDGRID_API_KEY: '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',
+    SENDGRID_API_KEY: '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',
     EMAIL_FROM: 'ecosystem.kazan@gmail.com',
 
     // EMAIL_OFFICE: 'turaltrueandfalse@gmail.com',
