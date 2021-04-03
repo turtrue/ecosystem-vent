5b999f4928f8b99ab32275ea1e4e3cc92f47f600
Tural Kerimov <turaltrueandfalse@gmail.com>
2021-04-03 20:17:22 +0300

    db

--- routes/catalog.js
+++ routes/catalog.js
@@ -2,42 +2,18 @@ const { Router } = require('express');
 const Category = require('../models/Category');
 const router = Router();
 
-router.get('/', (req, res) => {
-    try {
-        res.render('catalog', {
-            title: 'Каталог вентиляционного оборудования в Казани',
-            pageHeader: 'Каталог',
-            isCatalog: true
-        });
-    } catch (e) {
-        console.log(e);
-    }
-});
-
 function catalogRoute(category, translit) {
+    if (translit === 'katalog') translit = '';
+
     router.get(`/${translit}`, async (req, res) => {
-        console.log(`Отработал роут /${translit}`);
         try {
             const productData = await Category
-                .find({ 'translit': translit })
+                .find({ 'translit': translit || 'katalog' })
                 .populate('product');
 
-            const products = [];
-            productData.forEach(obj => {
-                products.push(obj.product);
-                // if (obj.product.isSubcategory) {
-                //     catalogRoute(obj.product.name, obj.product.translit);
-                // }
-            });
+            const products = productData.map(obj => obj.product);
 
-            // res.render(`catalog/catalog`, {
-            //     title: `${category} в Казани купить по выгодной цене от производителя`,
-            //     pageHeader: category,
-            //     isCatalog: true,
-            //     products
-            // });
-
-            res.render(`catalog/${translit}`, {
+            res.render('catalog', {
                 title: `${category} в Казани купить по выгодной цене от производителя`,
                 pageHeader: category,
                 isCatalog: true,
@@ -46,127 +22,34 @@ function catalogRoute(category, translit) {
         } catch (e) {
             console.log(e);
         }
+
+        console.log(`Отработал роут /${translit || 'catalog'}`);
     });
 }
 
 async function catalogRoutes() {
     try {
         const categoryData = await Category.find({});
-        const result = [];
-        categoryData.forEach(obj => result.push(obj.category));
-        const categories = new Set(result);
+        const setCategories = new Set();
+        const setTranslits = new Set();
 
-        for (category of categories) {
-            let translit;
-            for (let i = 0; i < categoryData.length; i++) {
-                if (categoryData[i].category === category) {
-                    translit = categoryData[i].translit;
-                    break;
-                }
-            }
-            catalogRoute(category, translit);
-        }
-        console.log('Роуты каталога перестроены');
+        categoryData.forEach(obj => {
+            setCategories.add(obj.category);
+            setTranslits.add(obj.translit);
+        });
+
+        const arrCategories = Array.from(setCategories);
+        const arrTranslits = Array.from(setTranslits);
+
+        arrTranslits.forEach((translit, i) => {
+            catalogRoute(arrCategories[i], translit);
+        });
     } catch (e) {
         console.log(e);
     }
+
+    console.log('Роуты каталога перестроены');
 }
 catalogRoutes();
-
-router.get('/vozdukhovody-i-fasonnye-chasti', (req, res) => {
-    try {
-        res.render('catalog/vozdukhovody-i-fasonnye-chasti', {
-            title: 'Воздуховоды и фасонные части в Казани купить по выгодной цене от производителя',
-            pageHeader: 'Воздуховоды и фасонные части',
-            isCatalog: true
-        });
-    } catch (e) {
-        console.log(e);
-    }
-});
-
-router.get('/diffuzory-i-reshetki', (req, res) => {
-    try {
-        res.render('catalog/diffuzory-i-reshetki', {
-            title: 'Диффузоры и решетки в Казани купить по выгодной цене от производителя',
-            pageHeader: 'Диффузоры и решетки',
-            isCatalog: true
-        });
-    } catch (e) {
-        console.log(e);
-    }
-});
-
-router.get('/vozdukhovody', (req, res) => {
-    try {
-        res.render('catalog/vozdukhovody', {
-            title: 'Воздуховоды в Казани купить по выгодной цене от производителя',
-            pageHeader: 'Воздуховоды',
-            isCatalog: true
-        });
-    } catch (e) {
-        console.log(e);
-    }
-});
-
-router.get('/fasonnye-chasti', (req, res) => {
-    try {
-        res.render('catalog/fasonnye-chasti', {
-            title: 'Фасонные части в Казани купить по выгодной цене от производителя',
-            pageHeader: 'Фасонные части',
-            isCatalog: true
-        });
-    } catch (e) {
-        console.log(e);
-    }
-});
-
-// router.get('/raskhodnye-materialy', (req, res) => {
-//     try {
-//         res.render('catalog/raskhodnye-materialy', {
-//             title: 'Расходные материалы в Казани купить по выгодной цене от производителя',
-//             pageHeader: 'Расходные материалы',
-//             isCatalog: true
-//         });
-//     } catch (e) {
-//         console.log(e);
-//     }
-// });
-
-router.get('/kondicionery', (req, res) => {
-    try {
-        res.render('catalog/kondicionery', {
-            title: 'Кондиционеры в Казани купить по выгодной цене от производителя',
-            pageHeader: 'Кондиционеры',
-            isCatalog: true
-        });
-    } catch (e) {
-        console.log(e);
-    }
-});
-
-router.get('/truby-i-fitingi', (req, res) => {
-    try {
-        res.render('catalog/truby-i-fitingi', {
-            title: 'Трубы и фитинги в Казани купить по выгодной цене от производителя',
-            pageHeader: 'Трубы и фитинги',
-            isCatalog: true
-        });
-    } catch (e) {
-        console.log(e);
-    }
-});
-
-router.get('/ognezashchita-i-teploizolyaciya', (req, res) => {
-    try {
-        res.render('catalog/ognezashchita-i-teploizolyaciya', {
-            title: 'Огнезащита и теплоизоляция в Казани купить по выгодной цене от производителя',
-            pageHeader: 'Огнезащита и теплоизоляция',
-            isCatalog: true
-        });
-    } catch (e) {
-        console.log(e);
-    }
-});
 
 module.exports = router;
